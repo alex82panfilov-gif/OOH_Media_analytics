d7d511efcd0db56497fcdb467dd265f450728589
alex82panfilov-gif <alex82panfilov@gmail.com>
2025-11-19 18:01:05 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Явно говорим, что файлы parquet — это ресурсы
-  assetsInclude: ['**/*.parquet'],
   build: {
-    // Убеждаемся, что выходная папка стандартная
     outDir: 'dist',
   },
-  // Убеждаемся, что пути работают от корня
   base: '/',
 });
