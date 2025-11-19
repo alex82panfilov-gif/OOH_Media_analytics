fd439af6bcd70ebaf40c038844a4d2c82a46bda4
alex82panfilov-gif <alex82panfilov@gmail.com>
2025-11-19 18:11:35 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,12 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Явно указываем папку public
+  publicDir: 'public',
   build: {
     outDir: 'dist',
+    // Отключаем лимит на размер чанков, чтобы не было желтых предупреждений
+    chunkSizeWarningLimit: 1600,
   },
   base: '/',
 });
